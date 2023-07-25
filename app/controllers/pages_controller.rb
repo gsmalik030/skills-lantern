c27f47495a0bc33bff61dc613f6f75f0e2f6180c
Malik Ghulam Subbhani <gsmalik030@gmail.com>
2023-07-25 18:16:30 +0200

    Changed pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home, :privacy_policy]
   def home
     @courses = Course.all.limit(3)
     @recent_courses = Course.all.limit(3).order(created_at: :desc)
