103cf55b6dee2afaea5f3cad82e38ea97bf8b8ee
Malik Ghulam Subbhani <gsmalik030@gmail.com>
2023-07-25 09:13:08 +0200

    Added home and privacy policy pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'privacy_policy', to: 'pages#privacy_policy'
+  root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
