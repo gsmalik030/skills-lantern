aeb8a56fe531b30e6d23ddd024d401fde2473b05
Malik Ghulam Subbhani <gsmalik030@gmail.com>
2023-07-25 14:33:29 +0200

    Created course model and controller

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,10 @@
 class Course < ApplicationRecord
+    belongs_to :user
+    validates :title, presence: true, length: { minimum: 5, maximum: 150 }
+    validates :description, presence: true, length: { minimum: 5 }
+    has_rich_text :description
+
+    def to_s
+        title
+    end
 end
