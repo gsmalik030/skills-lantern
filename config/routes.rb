0ead889773ed0a37db75dd41e826c0a800563df6
Malik Ghulam Subbhani <gsmalik030@gmail.com>
2023-07-25 11:21:51 +0200

    Created course scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :courses
   get 'privacy_policy', to: 'pages#privacy_policy'
   root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
