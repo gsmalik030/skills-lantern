76bfa9fd9db36addf8a9e1ef80aa42067a07986a
Malik Ghulam Subbhani <gsmalik030@gmail.com>
2023-07-26 07:35:56 +0200

    Added new fields to course in the views

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,10 @@ class Course < ApplicationRecord
     belongs_to :user
     validates :title, presence: true, length: { minimum: 5, maximum: 150 }
     validates :description, presence: true, length: { minimum: 5 }
+    validates :short_description, presence: true, length: { minimum: 5, maximum: 150 }
+    validates :language, :level, presence: true
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
+    
     has_rich_text :description
 
     def to_s
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,10 @@ User.create(email: 'test@test.com', password: 'password', password_confirmation:
     Course.create(
         title: Faker::Educator.course_name,
         description: Faker::TvShows::GameOfThrones.quote,
-        user_id: User.first.id
+        user_id: User.first.id,
+        short_description: Faker::TvShows::GameOfThrones.quote,
+        language: "English",
+        level: "Beginner",
+        price: 20.00
     )
 end
