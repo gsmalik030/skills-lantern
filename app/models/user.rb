5c38e715a234ce7cc188a13adb8cc2b8ea2deccc
Malik Ghulam Subbhani <gsmalik030@gmail.com>
2023-07-26 13:32:42 +0200

    Added user roles using rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  
   has_many :courses
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -11,10 +12,23 @@ class User < ApplicationRecord
     def self.ransackable_attributes(auth_object = nil)
       ["confirmation_sent_at", "confirmation_token", "confirmed_at", "created_at", "current_sign_in_at", "current_sign_in_ip", "email", "id", "last_sign_in_at", "last_sign_in_ip", "remember_created_at", "reset_password_sent_at", "reset_password_token", "sign_in_count", "unconfirmed_email", "updated_at"]
     end
-
+    rolify
     def username
       if email.present?
         email.split("@").first
+      end
+    end
+
+    after_create :assign_default_role
+
+    def assign_default_role
+      if User.count == 1
+        self.add_role(:admin) if self.roles.blank?
+        self.add_role(:teacher)
+        self.add_role(:student)
+      else
+        self.add_role(:student) if self.roles.blank?
+        self.add_role(:teacher)
       end
     end
 
