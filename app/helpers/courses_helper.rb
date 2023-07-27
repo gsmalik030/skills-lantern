8e8a81048ec72d07fcb51ba98d40e9aec0159788
Malik Ghulam Subbhani <gsmalik030@gmail.com>
2023-07-27 07:40:04 +0200

    Added a button to ask for review

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -10,4 +10,18 @@ module CoursesHelper
       link_to "Free", new_course_enrollment_path(course), class: "btn btn-sm btn-success"
     end
   end
+
+  def review_btn(course)
+    if current_user
+      user=course.enrollments.where(user: current_user)
+      if user.any?
+        if user.where(rating: [0, nil, ""], review: [0, nil, ""]).any?
+          link_to "Leave a Review", edit_enrollment_path(user.first), class: "btn btn-sm btn-outline-success"
+        else
+          "You already reviewed this course."
+          
+        end
+      end
+    end
+  end
 end
