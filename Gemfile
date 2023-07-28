1021ff7c21f41f27dd4abe4ff4908263ea36956b
Malik Ghulam Subbhani <gsmalik030@gmail.com>
2023-07-28 11:18:39 +0200

    Added impressions field to check how many time lesson is watched

--- Gemfile
+++ Gemfile
@@ -26,8 +26,6 @@ gem 'ransack'
 gem "rolify"
 gem "pundit", "~> 2.3"
 gem 'pagy'
-gem "chartkick"
-gem 'groupdate'
 
 group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_28_054406) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_28_085806) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -121,6 +121,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_28_054406) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "impressions", default: 0, null: false
     t.index ["lesson_id"], name: "index_user_lessons_on_lesson_id"
     t.index ["user_id"], name: "index_user_lessons_on_user_id"
   end
