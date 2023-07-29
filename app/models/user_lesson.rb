c061db911b029100435e6c55379b7ce5dc0166df
Malik Ghulam Subbhani <gsmalik030@gmail.com>
2023-07-29 16:45:01 +0200

    Updated lessons view

--- app/models/user_lesson.rb
+++ app/models/user_lesson.rb
@@ -1,6 +1,6 @@
 class UserLesson < ApplicationRecord
-  belongs_to :user
-  belongs_to :lesson
+  belongs_to :user, counter_cache: true
+  belongs_to :lesson, counter_cache: true
 
   validates :user, presence: true, if: -> { lesson.present? }
   validates :lesson, presence: true, if: -> { user.present? }
